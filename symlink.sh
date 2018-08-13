268e7c07f46df93e0ed1cad8d952e6b0ea30ac95
Trollstoy <444796+Trollstoy@users.noreply.github.com>
2018-08-13 18:33:07 -0400

    Removed symlink script

--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Modify the two vars so it match you own setup. Make sure you have it surrounded by double quotes
-# WoWRep  : World of warcraft main directory
-# GHRep   : Where your github projects are stored (by default in Documents/GitHub)
-WoWRep="/Applications/World of Warcraft"
-GHRep=$(pwd)
-
-ln -s "$GHRep" "$WoWRep/Interface/AddOns/NerdPack-Protected"
